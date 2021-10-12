peercoin/peercoin_flutter

allow data feeds to be enabled during setup

lib/screens/setup/setup_data_feeds.dart
@@ -1,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:peercoin/providers/appsettings.dart';
+import 'package:peercoin/providers/unencryptedOptions.dart';
+import 'package:peercoin/tools/app_localizations.dart';
+import 'package:peercoin/tools/app_routes.dart';
+import 'package:peercoin/widgets/buttons.dart';
+import 'package:peercoin/widgets/setup_progress.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SetupDataFeedsScreen extends StatefulWidget {
+  const SetupDataFeedsScreen({Key? key}) : super(key: key);
+
+  @override
+  _SetupDataFeedsScreenState createState() => _SetupDataFeedsScreenState();
+}
+
+class _SetupDataFeedsScreenState extends State<SetupDataFeedsScreen> {
+  void _launchURL(_url) async {
+    await canLaunch(_url)
+        ? await launch(
+            _url,
+          )
+        : throw 'Could not launch $_url';
+  }
+
+  bool _dataFeedAllowed = false;
+  bool _initial = true;
+  late AppSettings _settings;
+
+  @override
+  void didChangeDependencies() async {
+    if (_initial) {
+      _settings = Provider.of<AppSettings>(context, listen: false);
+      await _settings.init();
+      setState(() {
+        _initial = false;
+      });
+    }
+    super.didChangeDependencies();
+  }
+
+  void toggleHandler(bool newState) {
+    _settings.setSelectedCurrency(newState == true ? 'USD' : '');
+
+    setState(() {
+      _dataFeedAllowed = newState;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: SetupProgressIndicator(4),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon/ppc-icon-white-256.png',
+              width: 50,
+            ),
+            Text(
+              AppLocalizations.instance.translate('setup_price_feed_title'),
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+            Text(
+              AppLocalizations.instance
+                  .translate('setup_price_feed_description'),
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SwitchListTile(
+                title: Text(
+                    AppLocalizations.instance
+                        .translate('setup_price_feed_allow'),
+                    style: TextStyle(color: Colors.white)),
+                value: _dataFeedAllowed,
+                activeColor: Colors.white,
+                inactiveThumbColor: Colors.grey,
+                onChanged: (newState) => toggleHandler(newState)),
+            PeerButton(
+              action: () => _launchURL(
+                  'https://github.com/peercoin/peercoin_flutter/blob/main/data_protection.md'),
+              text:
+                  AppLocalizations.instance.translate('about_data_declaration'),
+            ),
+            PeerButtonBorder(
+              text: AppLocalizations.instance.translate('setup_finish'),
+              action: () async {
+                var prefs = await Provider.of<UnencryptedOptions>(context,
+                        listen: false)
+                    .prefs;
+                await prefs.setBool('setupFinished', true);
+                await Navigator.of(context)
+                    .pushNamedAndRemoveUntil(Routes.WalletList, (_) => false);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

lib/tools/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:peercoin/screens/app_settings_screen.dart';
 import 'package:peercoin/screens/auth_jail.dart';
+import 'package:peercoin/screens/setup/setup_data_feeds.dart';
 import 'package:peercoin/screens/wallet/import_paper_wallet.dart';
 import 'package:peercoin/screens/qrcodescanner.dart';
 import 'package:peercoin/screens/server_add.dart';
@@ -23,6 +24,7 @@ class Routes {
   static const String SetupScreen = '/setup-save-seed';
   static const String SetupImport = '/setup-import-seed';
   static const String SetupLanguage = '/setup-language';
+  static const String SetupDataFeeds = '/setup-feeds';
   static const String Transaction = '/tx-detail';
   static const String WalletHome = '/wallet-home';
   static const String WalletImportScan = '/wallet-import-scan';
@@ -47,6 +49,7 @@ class Routes {
       Routes.AuthJail: (context) => AuthJailScreen(),
       Routes.ServerSettings: (context) => ServerSettingsScreen(),
       Routes.ServerAdd: (context) => ServerAddScreen(),
+      Routes.SetupDataFeeds: (context) => SetupDataFeedsScreen(),
     };
   }
 }

reddcoin-project/reddcoin_flutter

lib/screens/setup/setup_data_feeds.dart
@@ -1,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:reddcoin/providers/appsettings.dart';
+import 'package:reddcoin/providers/unencryptedOptions.dart';
+import 'package:reddcoin/tools/app_localizations.dart';
+import 'package:reddcoin/tools/app_routes.dart';
+import 'package:reddcoin/widgets/buttons.dart';
+import 'package:reddcoin/widgets/setup_progress.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SetupDataFeedsScreen extends StatefulWidget {
+  const SetupDataFeedsScreen({Key? key}) : super(key: key);
+
+  @override
+  _SetupDataFeedsScreenState createState() => _SetupDataFeedsScreenState();
+}
+
+class _SetupDataFeedsScreenState extends State<SetupDataFeedsScreen> {
+  void _launchURL(_url) async {
+    await canLaunch(_url)
+        ? await launch(
+            _url,
+          )
+        : throw 'Could not launch $_url';
+  }
+
+  bool _dataFeedAllowed = false;
+  bool _initial = true;
+  late AppSettings _settings;
+
+  @override
+  void didChangeDependencies() async {
+    if (_initial) {
+      _settings = Provider.of<AppSettings>(context, listen: false);
+      await _settings.init();
+      setState(() {
+        _initial = false;
+      });
+    }
+    super.didChangeDependencies();
+  }
+
+  void toggleHandler(bool newState) {
+    _settings.setSelectedCurrency(newState == true ? 'USD' : '');
+
+    setState(() {
+      _dataFeedAllowed = newState;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: SetupProgressIndicator(4),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon/ppc-icon-white-256.png',
+              width: 50,
+            ),
+            Text(
+              AppLocalizations.instance.translate('setup_price_feed_title'),
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+            Text(
+              AppLocalizations.instance
+                  .translate('setup_price_feed_description'),
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SwitchListTile(
+                title: Text(
+                    AppLocalizations.instance
+                        .translate('setup_price_feed_allow'),
+                    style: TextStyle(color: Colors.white)),
+                value: _dataFeedAllowed,
+                activeColor: Colors.white,
+                inactiveThumbColor: Colors.grey,
+                onChanged: (newState) => toggleHandler(newState)),
+            PeerButton(
+              action: () => _launchURL(
+                  'https://github.com/reddcoin-project/reddcoin_flutter/blob/main/data_protection.md'),
+              text:
+                  AppLocalizations.instance.translate('about_data_declaration'),
+            ),
+            PeerButtonBorder(
+              text: AppLocalizations.instance.translate('setup_finish'),
+              action: () async {
+                var prefs = await Provider.of<UnencryptedOptions>(context,
+                        listen: false)
+                    .prefs;
+                await prefs.setBool('setupFinished', true);
+                await Navigator.of(context)
+                    .pushNamedAndRemoveUntil(Routes.WalletList, (_) => false);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

lib/tools/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:reddcoin/screens/app_settings_screen.dart';
 import 'package:reddcoin/screens/auth_jail.dart';
+import 'package:reddcoin/screens/setup/setup_data_feeds.dart';
 import 'package:reddcoin/screens/wallet/import_paper_wallet.dart';
 import 'package:reddcoin/screens/qrcodescanner.dart';
 import 'package:reddcoin/screens/server_add.dart';
@@ -23,6 +24,7 @@ class Routes {
   static const String SetupScreen = '/setup-save-seed';
   static const String SetupImport = '/setup-import-seed';
   static const String SetupLanguage = '/setup-language';
+  static const String SetupDataFeeds = '/setup-feeds';
   static const String Transaction = '/tx-detail';
   static const String WalletHome = '/wallet-home';
   static const String WalletImportScan = '/wallet-import-scan';
@@ -47,6 +49,7 @@ class Routes {
       Routes.AuthJail: (context) => AuthJailScreen(),
       Routes.ServerSettings: (context) => ServerSettingsScreen(),
       Routes.ServerAdd: (context) => ServerAddScreen(),
+      Routes.SetupDataFeeds: (context) => SetupDataFeedsScreen(),
     };
   }
 }